b05ff1d0f64e4ac7108d267287e1f9abe3493e4f
chengr <sprry9560@gmail.com>
2015-03-04 19:11:42 +0800

    https://github.com/chengr/LFD_TD.git

--- LFD_TD/Source.cpp
+++ LFD_TD/Source.cpp
@@ -3,7 +3,6 @@
 #include "opencv2/imgproc/imgproc.hpp"
 #include <cv.h>
 #include <stdio.h>
-
 #include <fstream> 
 using namespace cv;
 using namespace std;
@@ -11,7 +10,6 @@ double pi =3.141592653589793;
 Mat find_circle_and_draw_rec(Mat src_img,int size);//輸入影像,輸出影像大小
 int main(int argc, char** argv)
 {
-	//
 	Mat src;
 	Mat result;
 	int num=657;
@@ -50,13 +48,7 @@ Mat find_circle_and_draw_rec(Mat src_img,int size){
 	Sobel( src_gray, grad_y, ddepth, 0, 1, 3, scale, delta, BORDER_DEFAULT );
 	convertScaleAbs( grad_y, abs_grad_y );
 	addWeighted( abs_grad_x, 0.5, abs_grad_y, 0.5, 0, src_gray );
-	//namedWindow( "sobel", CV_WINDOW_AUTOSIZE );
-	//imshow("sobel",src_gray);
-	/// Reduce the noise so we avoid false circle detection
 	GaussianBlur( src_gray,src_gray, Size(9,9), 2, 2 );
-	//namedWindow( "blur", CV_WINDOW_AUTOSIZE );
-	//imshow("blur",src_gray);
-	//find circle
 	vector<Vec3f> circles;
 	/// Apply the Hough Transform to find the circles 
 	HoughCircles(src_gray, circles, CV_HOUGH_GRADIENT,1,src_gray.rows/4, 200, 100, 900, 1100 );
@@ -79,11 +71,6 @@ Mat find_circle_and_draw_rec(Mat src_img,int size){
 	double k=cvRound(circles[0][1]);
 	double r=cvRound(circles[0][2]);//radius
 	double h2=0,k2=0;
-	//x=Xm+r*cos(theata) y=Ym+r*sin(theata) 
-	//(Xm,Ym)為圓心
-	//r為半徑
-
-
 	double dd=360/(double)size;
 	for(int i=0;i<size;i++){
 		double deg=(i*dd*pi)/180;
@@ -96,36 +83,5 @@ Mat find_circle_and_draw_rec(Mat src_img,int size){
 		}
 
 	}
-
-	/*
-	for(int i=0;i<size;i++){
-		double deg=(i*dd*pi)/180;
-		x=h+r*cos(deg);
-		y=k+r*sin(deg);
-		double a=(y-k)/(x-h);//y=ax; x=y/a;
-		if(abs(a)>=1){//看y
-			double ds=(y-k)/(double)size;
-			double tx=0;
-			double ty=0;
-			for(int j=0;j<size;j++){
-				ty=k+j*ds;
-				tx=h+j*ds/a;
-				Vec3b color=src_img.at<Vec3b>(Point(tx,ty));
-				result.at<Vec3b>(Point(i,j)) = color;
-			}
-		}
-		else{//看X
-			double ds=(x-h)/(double)size;
-			double tx=0;
-			double ty=0;
-			for(int j=0;j<size;j++){
-				tx=h+j*ds;
-				ty=k+j*ds*a;
-				Vec3b color=src_img.at<Vec3b>(Point(tx,ty));
-				result.at<Vec3b>(Point(i,j)) = color;
-			}
-		}
-	}*/
-	//GaussianBlur( result,result, Size(9,9), 2, 2 );
 	return result;
 }
